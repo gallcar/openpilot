29485b9f62facf830c7bc0a8c1587e2e07442b01
stonerains <72440538+stonerains@users.noreply.github.com>
2021-04-11 00:24:12 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -204,9 +204,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneV = [0., 0., 0.015]
 
       ret.gasMaxBP = [0., 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 36.*CV.KPH_TO_MS, 37.*CV.KPH_TO_MS, 48.*CV.KPH_TO_MS, 55.*CV.KPH_TO_MS, 65.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-      ret.gasMaxV = [0.45, 0.61, 0.5608, 0.43, 0.38, 0.1805, 0.13, 0.13, 0.2,  0.22, 0.16] 
+      ret.gasMaxV = [0.45, 0.61, 0.5607, 0.43, 0.38, 0.1805, 0.13, 0.13, 0.198, 0.22, 0.16] 
       ret.brakeMaxBP = [0., 29.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 50*CV.KPH_TO_MS, 65*CV.KPH_TO_MS]
-      ret.brakeMaxV = [0.754, 0.854, 0.8669, 0.85615, 0.84]
+      ret.brakeMaxV = [0.754, 0.854, 0.8669, 0.85615, 0.841]
 
       ret.stoppingBrakeRate = 0.155  # brake_travel/s while trying to stop
       ret.startingBrakeRate = 0.99  # brake_travel/s while releasing on restart
