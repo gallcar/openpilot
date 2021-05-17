023b91b4a8e14931d382a760b987dafb772e030f
stonerains <72440538+stonerains@users.noreply.github.com>
2021-05-17 15:11:37 +0900

    Update paint.cc
    스위치 변경

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -697,12 +697,12 @@ static void bb_ui_draw_UI(UIState *s)
   const int bb_dmr_y = (box_y + (bdr_is * 1.5)) + UI_FEATURE_RIGHT_Y;
 // 스위치 화면 비우기 시작
 #if UI_FEATURE_LEFT
-  if(s->show_debug_ui)
+  if(!s->Fuse_With_StockScc)
     bb_ui_draw_measures_left(s, bb_dml_x, bb_dml_y, bb_dml_w);
 #endif
 
 #if UI_FEATURE_RIGHT
-  if(s->show_debug_ui)
+  if(!s->Fuse_With_StockScc)
     bb_ui_draw_measures_right(s, bb_dmr_x, bb_dmr_y, bb_dmr_w);
 #endif
 
