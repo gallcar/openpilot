8131db252c076f44c345edd7146144077addde53
stonerains <72440538+stonerains@users.noreply.github.com>
2021-07-01 16:56:02 +0900

    Update interface.py
    eq900 엔튠값으로 기본값

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -51,7 +51,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.lateralTuning.lqr.scale = 1680.
     ret.lateralTuning.lqr.ki = 0.01
-    ret.lateralTuning.lqr.dcGain = 0.002858
+    ret.lateralTuning.lqr.dcGain = 0.002856
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -60,7 +60,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
 
     ret.steerRatio = 17.2
-    ret.steerActuatorDelay = 0.1
+    ret.steerActuatorDelay = 0.97
     ret.steerLimitTimer = 3.0
     ret.steerRateCost = 0.4
     ret.steerMaxBP = [0.]
