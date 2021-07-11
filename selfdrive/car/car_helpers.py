e4918bfb889ae7cc7c6b8d488c0b78efca72bedd
gallcar <76591391+gallcar@users.noreply.github.com>
2021-07-11 20:51:28 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -180,7 +180,7 @@ def get_car(logcan, sendcan):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "KIA STINGER GT2 2018"
   
   if Params().get("CarModel", encoding="utf8") is not None:
     car_name = Params().get("CarModel", encoding="utf8")
